6c99abec850eeefff73192150720558f81e4bb3c
TheD3luxed <karol.michalski95@gmail.com>
2018-12-21 15:07:28 +0100

    MainViewModel: delete saved uid on logout

--- app/src/main/java/pl/karolmichalski/shareshopping/presentation/screens/main/MainViewModel.kt
+++ app/src/main/java/pl/karolmichalski/shareshopping/presentation/screens/main/MainViewModel.kt
@@ -2,12 +2,17 @@ package pl.karolmichalski.shareshopping.presentation.screens.main
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import pl.karolmichalski.shareshopping.data.SharedPrefs
+import javax.inject.Inject
 
 class MainViewModel : ViewModel() {
 
 	val drawerOpened = MutableLiveData<Boolean>().apply { value = false }
 
-	fun logOut() {
+	@Inject
+	lateinit var sharedPrefs: SharedPrefs
 
+	fun logOut() {
+		sharedPrefs.saveUid("")
 	}
 }
